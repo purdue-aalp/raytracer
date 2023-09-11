674580a4bb1483129bbc7dd15656fca910bd8b86
Fangjia Shen <shen449@purdue.edu>
2023-09-11 18:38:12 -0400

    Ray and AABB can be either 32-bit or 33-bit float formats

--- src/main/scala/baseline_datapath/Datapath.scala
+++ src/main/scala/baseline_datapath/Datapath.scala
@@ -4,37 +4,39 @@ import chisel3._
 import hardfloat._
 
 class Datapath extends Module {
-  val ray = IO(Input(new Ray()))
-  val aabb = IO(Input(new AABB()))
+  val ray = IO(Input(new Ray(recorded_float = false)))
+  val aabb = IO(Input(new AABB(recorded_float = false)))
   val sum = IO(Output(Bits(32.W)))
+
+  // convert ieee float input to recorded float
+  val ray_rec = RayConvertFNtoRecFN(ray)
+  val aabb_rec = AABBConvertFNtoRecFN(aabb)
 
   val flattened_inputs = VecInit(
     Seq(
-      ray.dir.x,
-      ray.dir.y,
-      ray.dir.z,
-      ray.origin.x,
-      ray.origin.y,
-      ray.origin.z,
-      ray.inv.x,
-      ray.inv.y,
-      ray.inv.z,
-      ray.extent,
-      aabb.x_max,
-      aabb.x_min,
-      aabb.y_max,
-      aabb.y_min,
-      aabb.z_max,
-      aabb.z_min
+      ray_rec.dir.x,
+      ray_rec.dir.y,
+      ray_rec.dir.z,
+      ray_rec.origin.x,
+      ray_rec.origin.y,
+      ray_rec.origin.z,
+      ray_rec.inv.x,
+      ray_rec.inv.y,
+      ray_rec.inv.z,
+      ray_rec.extent,
+      aabb_rec.x_max,
+      aabb_rec.x_min,
+      aabb_rec.y_max,
+      aabb_rec.y_min,
+      aabb_rec.z_max,
+      aabb_rec.z_min
     )
   )
-
-  val recFNInputs = flattened_inputs.map(recFNFromFN(8, 24, _))
 
   // RecFN's exponent has an additional bit
   val recFNSum = Wire(Bits(33.W))
 
-  recFNSum := recFNInputs.tail.foldLeft(recFNInputs.head){(x,y) => 
+  recFNSum := flattened_inputs.tail.foldLeft(flattened_inputs.head){(x,y) => 
       // RecFN's exponent has an additional bit
       val fu = Module(new AddRecFN(8+1, 24))
       fu.io.subOp := false.B
--- src/main/scala/baseline_datapath/IOSpec.scala
+++ src/main/scala/baseline_datapath/IOSpec.scala
@@ -1,37 +1,141 @@
 package baseline_datapath
 
 import chisel3._
+import hardfloat.{recFNFromFN, fNFromRecFN}
 
-class Float3 extends Bundle {
+class Float3 (recorded_float: Boolean = false) extends Bundle {
   /**
     * Defines a point in three-dimensional space.
-    * All values are IEEE-754 floats (32 bits).
+    * With "recorded_float" being false, the expected format is IEEE-754 32-bit float
+    * With "recorded_float" being true, the expected format is (1 bit sign + 9
+    * bit exponent + 23 bit fraction). This is the 33-bit long "recorded float"
+    * format used by the HardFloat library, that corresponds to the 32-bit
+    * standard float.
     */
-  val x = Bits(32.W)
-  val y = Bits(32.W)
-  val z = Bits(32.W)
+  val bit_width = if(recorded_float) 33.W else 32.W
+  
+  val x = Bits(bit_width)
+  val y = Bits(bit_width)
+  val z = Bits(bit_width)
+
+  def isRecordedFloat(): Boolean = {recorded_float}
 }
 
-class Ray extends Bundle {
+class Ray (recorded_float: Boolean = false) extends Bundle {
   /**
     * A ray in three-dimensional space.
     */
-  val origin = new Float3()
-  val dir = new Float3()
-  val inv = new Float3()
-  val extent = Bits(32.W)
+  val origin = new Float3(recorded_float)
+  val dir = new Float3(recorded_float)
+  val inv = new Float3(recorded_float)
+
+  val _bit_width = if(recorded_float) 33.W else 32.W
+  val extent = Bits(_bit_width)
+
+  def isRecordedFloat(): Boolean = {recorded_float}
 }
 
-class AABB extends Bundle {
+class AABB (recorded_float: Boolean = false) extends Bundle {
   /**
     * Defines an Axis-Aligned Bounding Box (AABB) in three-dimensional space.
-    * All values are IEEE-754 floats (32 bits) .
+    * All values are either IEEE-754 floats (!recorded_float) or 33-bit recorded
+    * floats .
     */
-  val x_min = Bits(32.W)
-  val y_min = Bits(32.W)
-  val z_min = Bits(32.W)
+
+  val bit_width = if(recorded_float) 33.W else 32.W
+
+  val x_min = Bits(bit_width)
+  val y_min = Bits(bit_width)
+  val z_min = Bits(bit_width)
   
-  val x_max = Bits(32.W)
-  val y_max = Bits(32.W)
-  val z_max = Bits(32.W)
+  val x_max = Bits(bit_width)
+  val y_max = Bits(bit_width)
+  val z_max = Bits(bit_width)
+
+  def isRecordedFloat(): Boolean = {recorded_float} 
+}
+
+
+// Conversion circuits between 32-bit IEEE float and 33-bit recorded float
+
+object RayConvertFNtoRecFN{
+  def apply(in: Ray): Ray = {
+    if(in.isRecordedFloat()){
+      throw new Exception("Ray must be in IEEE-754 format before it can be converted to recorded format")
+    }
+
+    val out = Wire(new Ray(recorded_float = true))
+    out.origin.x := recFNFromFN(8, 24, in.origin.x)
+    out.origin.y := recFNFromFN(8, 24, in.origin.y)
+    out.origin.z := recFNFromFN(8, 24, in.origin.z)
+    out.dir.x := recFNFromFN(8, 24, in.dir.x)
+    out.dir.y := recFNFromFN(8, 24, in.dir.y)
+    out.dir.z := recFNFromFN(8, 24, in.dir.z)
+    out.inv.x := recFNFromFN(8, 24, in.inv.x)
+    out.inv.y := recFNFromFN(8, 24, in.inv.y)
+    out.inv.z := recFNFromFN(8, 24, in.inv.z)
+    out.extent := recFNFromFN(8, 24, in.extent)
+
+    out
+  }
+}
+
+object RayConvertRecFNtoFN{
+  def apply(in: Ray): Ray = {
+    if(in.isRecordedFloat() == false){
+      throw new Exception("Ray must be in recorded format before it can be converted to IEEE-754 format")
+    }
+
+    val out = Wire(new Ray(recorded_float = false))
+    out.origin.x := fNFromRecFN(8, 24, in.origin.x)
+    out.origin.y := fNFromRecFN(8, 24, in.origin.y)
+    out.origin.z := fNFromRecFN(8, 24, in.origin.z)
+    out.dir.x := fNFromRecFN(8, 24, in.dir.x)
+    out.dir.y := fNFromRecFN(8, 24, in.dir.y)
+    out.dir.z := fNFromRecFN(8, 24, in.dir.z)
+    out.inv.x := fNFromRecFN(8, 24, in.inv.x)
+    out.inv.y := fNFromRecFN(8, 24, in.inv.y)
+    out.inv.z := fNFromRecFN(8, 24, in.inv.z)
+    out.extent := fNFromRecFN(8, 24, in.extent)
+
+    out
+  }
+}
+
+object AABBConvertFNtoRecFN{
+  def apply(in: AABB): AABB = {
+    if(in.isRecordedFloat()){
+      throw new Exception("AABB must be in IEEE-754 format before it can be converted to recorded format")
+    }
+
+    val out = Wire(new AABB(recorded_float = true))
+    out.x_min := recFNFromFN(8, 24, in.x_min)
+    out.y_min := recFNFromFN(8, 24, in.y_min)
+    out.z_min := recFNFromFN(8, 24, in.z_min)
+
+    out.x_max := recFNFromFN(8, 24, in.x_max)
+    out.y_max := recFNFromFN(8, 24, in.y_max)
+    out.z_max := recFNFromFN(8, 24, in.z_max)
+
+    out
+  }
+}
+
+object AABBConvertRecFNtoFN{
+  def apply(in: AABB): AABB = {
+    if(in.isRecordedFloat() == false){
+      throw new Exception("AABB must be in recorded format before it can be converted to IEEE-754 format")
+    }
+
+    val out = Wire(new AABB(recorded_float = true))
+    out.x_min := fNFromRecFN(8, 24, in.x_min)
+    out.y_min := fNFromRecFN(8, 24, in.y_min)
+    out.z_min := fNFromRecFN(8, 24, in.z_min)
+
+    out.x_max := fNFromRecFN(8, 24, in.x_max)
+    out.y_max := fNFromRecFN(8, 24, in.y_max)
+    out.z_max := fNFromRecFN(8, 24, in.z_max)
+
+    out
+  }
 }
