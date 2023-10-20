d5120fd47f7cd31154a2d7ed154ed1ce5543e08a
Fangjia Shen <shen449@purdue.edu>
2023-10-20 17:24:07 -0400

    unimportant

--- src/main/scala/baseline_datapath/EmitVerilog.scala
+++ src/main/scala/baseline_datapath/EmitVerilog.scala
@@ -2,15 +2,25 @@ package baseline_datapath
 
 import chisel3._
 import circt.stage.ChiselStage
+import hardfloat._
 
 class Foo extends Module {
-  val in = IO(Input(Bool()))
-  val in2 = IO(Input(Bool()))
-  val out = IO(Output(Bool()))
+  val in = IO(Input(Bits(33.W)))
+  val in2 = IO(Input(Bits(33.W)))
+  // val out = IO(Output(new RawFloat(8, 24)))
+  val out2 = IO(Output(Bits(33.W)))
 
-  val inter = Wire(Bool())
-  inter := in
-  out := inter & in2
+  // val inter = WireDefault(3.U(2.W))
+  // out := resizeRawFloat(8, 22, in)
+  // out := rawFloatFromRecFN(8, 24, in2)
+
+  val fu = Module(new AddRecFN(8, 24))
+  fu.io.a := in 
+  fu.io.b := in2
+  fu.io.subOp := false.B
+  fu.io.roundingMode := consts.round_near_even
+  fu.io.detectTininess := consts.tininess_beforeRounding
+  out2 := fu.io.out
 }
 
 object EmitVerilog extends App {
---
+++ src/main/scala/baseline_datapath/FU_summary.md
@@ -0,0 +1,14 @@
+# FU usage summary
+
+| Stage | FU used by Raytriangle | FU used by Raybox |
+| --- | --- | --- |
+| 1 | NA | NA |
+|2 | NA | NA |
+|3|9 Adders|**24 Adders**|
+|4|9 MulAdds | **24 MulAdds**|
+|5|**6 Multipliers**| 0 |
+|6|**3 Adders**|0|
+|7|**3 Multipliers**|0|
+|8|**2 Adders**|0|
+|9|**2 Adders**|0|
+|10|NA|NA|
